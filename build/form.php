aade26e6dde6edb5283680cba922e6cc9a6d54cd
Konstantin Bogdanov <kbogdanov1@gmail.com>
2017-10-06 16:55:10 +0300

    test commit

--- build/form.php
+++ build/form.php
@@ -3,20 +3,17 @@
 <?php
 
 
-
-    if(isset($_POST['name']) && isset($_POST['phone']))
-    {
+//     if(isset($_POST['name']) && isset($_POST['phone']))
+//     {
         // Send email
-        // $name = htmlspecialchars($_POST["name"]);
-        // $phone = htmlspecialchars($_POST["phone"]);
-        $name = $_POST["name"];
-        $phone = $_POST["phone"];
+        $name = htmlspecialchars($_POST["name"]);
+        $phone = htmlspecialchars($_POST["telephone"]);
         $to = "kbogdanov1@gmail.com";
 //         $from = "From: ".$name."\r\n";
         $subject = "Новая заявка на зеркало";
-        $message = "Поступила новая заявка на заказ зеркала \n\n Имя клиента: ".$name. " \n\n Телефон клиента: ".$phone;
+        $message = "Поступила новая заявка от на заказ зеркала \n\n Имя клиента: ".$name. " \n\n Телефон клиента: ".$phone;
 //         mail($to, $subject, $message, $from);
         mail($to, $subject, $message);
-    }
+//     }
 
 ?>
